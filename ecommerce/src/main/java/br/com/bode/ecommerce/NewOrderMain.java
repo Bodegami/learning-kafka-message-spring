a581b660914702ff223a99409066bbe0c85ba5fb
bodegami <rena.f.oliveira@gmail.com>
2022-07-15 23:59:16 -0300

    Criando varios consumidores e produtores com Java

--- ecommerce/src/main/java/br/com/bode/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/bode/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.bode.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -14,14 +15,18 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "12435, 54785, 2541635";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition "
                     + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
